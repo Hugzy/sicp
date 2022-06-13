750b1f6f95ba53e5dc3b0bcecb3313e9a3cca0ca
Frederik M <fhm@digizuite.com>
2022-06-13 09:16:05 +0200

    started on ex140, some recurssion issue

---
+++ lib/ex140.ex
@@ -0,0 +1,25 @@
+defmodule Ex140 do
+  @dx 0.00001
+
+  def sqrt(x) do
+    nm(fn y -> ((square(y)) - x) end, 1.0)
+  end
+
+  def square(x) do
+    x * x
+  end
+
+  def nm(g, guess) do
+    Ex135.fixed_point((newton_transform g), guess)
+  end
+
+  defp newton_transform(g) do
+    fn x -> (g.(x) / (deriv(g).(x))) - x end
+  end
+
+  defp deriv(g) do
+    fn x ->
+      (g.((x + @dx)) - g.(x)) / @dx
+    end
+  end
+end
